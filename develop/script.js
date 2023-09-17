b063005728cf95a9cb40a6f2102b66448e4adb36
Chiko repo <124736255+chikogit@users.noreply.github.com>
2023-09-17 17:51:16 -0400

    HTML-update

--- develop/script.js
+++ develop/script.js
@@ -1,23 +1,58 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
+// document function presents current day id with the date format using moment.js, 
+// day.js is implemented to illustrate the format and is mainly used for the time retrieval of the getCurrentHour function.
+// the save button is implemented with its variables attached by the description from the text-area part of the planner 
+// and the id representing the time hour. both are set to a user's local storage by the set item added on.
+
+
+$(document).ready(function() {
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a")); //moment.js
+
+    dayjs("#currentDay").format("{YYYY} MM-DDTHH:mm:ss SSS [Z] A"); //day.js
+    
+    $(".saveBtn").on("click",function() {
+    var savedTaskToDo = $(this).siblings(".description").val();
+    var timeDue = $(this).parent().attr("id");
+
+    localStorage.setItem(timeDue, savedTaskToDo);
+})
+
+//retrieval of stored timehour elements saved on the user's local storage using getitem
+$("#timehour-8 .description").val(localStorage.getItem("timehour-8"));
+$("#timehour-9 .description").val(localStorage.getItem("timehour-9"));
+$("#timehour-10 .description").val(localStorage.getItem("timehour-10"));
+$("#timehour-11 .description").val(localStorage.getItem("timehour-11"));
+$("#timehour-12 .description").val(localStorage.getItem("timehour-12"));
+$("#timehour-13 .description").val(localStorage.getItem("timehour-13"));
+$("#timehour-14 .description").val(localStorage.getItem("timehour-14"));
+$("#timehour-15 .description").val(localStorage.getItem("timehour-15"));
+$("#timehour-16 .description").val(localStorage.getItem("timehour-16"));
+$("#timehour-17 .description").val(localStorage.getItem("timehour-17"));
+
+
+//rest of function runs on day.js and jquery function. current hour function retrieves current time of the work day by hour in a 24-hour clock time format, 
+//implements css elements that determine whether a to-do is late ("past"),
+//due ("present"), and to be done by ("future"). function reruns at the end,
+function getCurrentHour() {
+    var currentHour = dayjs().hour(); //day.js
+
+    $(".time-block").each(function () { //jquery
+        var hourOfWorkTime = parseInt($(this).attr("id").split("timehour-")[1]); 
+
+        if (hourOfWorkTime < currentHour) {
+            $(this).addClass("past");
+        }
+       else if (hourOfWorkTime === currentHour) {
+           $(this).removeClass("past");
+           $(this).addClass("present");
+       }
+       else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+       }
+     })
+    }
+
+getCurrentHour();
+
+});
